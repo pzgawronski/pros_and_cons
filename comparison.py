f9db0a513c47eb980679c09d8fdf82fb65da2d5a
Patryk Gawronski <pzgawronski@gmail.com>
2025-02-19 18:48:54 +0100

    Added a Collection class (WIP)

---
+++ comparison.py
@@ -0,0 +1,7 @@
+
+
+
+class Comparison:
+
+    def __init__(self, title):
+        # TO DO
--- elements.py
+++ elements.py
@@ -1,6 +1,6 @@
 class Element:
 
-    def __init__(self, title: str, is_pro: bool = True, rating: int = 5, comment=None):
+    def __init__(self, title: str, is_pro: bool, rating: int, comment: str):
         self.title = title
         self.is_pro = is_pro
         self.rating = rating
@@ -14,9 +14,6 @@ class Element:
         pro_con_status = "PRO" if self.is_pro is True else "CON"
         return pro_con_status
 
-    def flip_pro_con(self):
-        self.is_pro = not self.is_pro
-
     def change_rating(self, new_rating: int):
         if new_rating not in range(1, 11):
             print("New rating is out of range. Please submit a rating on a scale of 1 to 10.")
@@ -24,34 +21,51 @@ class Element:
             self.rating = new_rating
 
 
+class Pro(Element):
+
+    def __init__(self, title, rating=5, comment=None):
+        super().__init__(title=title, rating=rating, comment=comment, is_pro=True)
+
+
+class Con(Element):
+
+    def __init__(self, title, rating=5, comment=None):
+        super().__init__(title=title, rating=rating, comment=comment, is_pro=False)
+
+
 if __name__ == "__main__":
 
     def test_display(test_element_display: Element):
         return f"{test_element_display.get_pro_or_con()}\t{test_element_display}"
 
-    test_pro = Element("test_pro_2", True, rating=2)
-    test_con = Element("test_con_2", False, rating=2)
+    test_pro = Pro("test_pro_2", rating=2)
+    test_con = Con("test_con_2", rating=2)
 
-    test_pro_comment = Element("test_pro_comment", True, rating=4, comment="Test comment")
-    test_con_comment = Element("test_con_comment", False, rating=4, comment="Test comment")
+    test_pro_comment = Pro("test_pro_comment", rating=4, comment="Test comment")
+    test_con_comment = Con("test_con_comment", rating=4, comment="Test comment")
 
     for test_element in [test_pro, test_con, test_pro_comment, test_con_comment]:
         print(test_display(test_element))
 
-    test_pro_cond = Element("test_pro_cond", True, rating=6)
-    test_con_prod = Element("test_con_prod", False, rating=6)
+    test_pro_cond = Pro("test_pro_cond", rating=6)
+    test_con_prod = Con("test_con_prod", rating=6)
 
-    for test_element in [test_pro_cond, test_con_prod]:
-        print(test_display(test_element))
-        test_element.flip_pro_con()
-        print(test_display(test_element))
-
-    test_pro_2_to_4 = Element("test_pro_2", True, rating=2)
+    test_pro_2_to_4 = Pro("test_pro_2_to_4", rating=2)
     print(test_display(test_pro_2_to_4))
     test_pro_2_to_4.change_rating(4)
     print(test_display(test_pro_2_to_4))
 
-    test_pro_2_to_11 = Element("test_pro_2", True, rating=2)
-    print(test_display(test_pro_2_to_4))
+    test_con_2_to_4 = Pro("test_con_2_to_4", rating=2)
+    print(test_display(test_con_2_to_4))
+    test_con_2_to_4.change_rating(4)
+    print(test_display(test_con_2_to_4))
+
+    test_pro_2_to_11 = Pro("test_pro_2_to_11", rating=2)
+    print(test_display(test_con_2_to_4))
     test_pro_2_to_11.change_rating(11)
-    print(test_display(test_pro_2_to_4))
+    print(test_display(test_con_2_to_4))
+
+    test_con_2_to_11 = Pro("test_con_2_to_11", rating=2)
+    print(test_display(test_con_2_to_4))
+    test_con_2_to_11.change_rating(11)
+    print(test_display(test_con_2_to_4))
